419595c78ce9f5c55c2057fef4318b5751c7b3c0
Shubham <tsp@outlook.in>
2020-03-12 12:02:10 +0530

    modified useeffect.js file

--- client/src/apps/training/components/UseEffect.jsx
+++ client/src/apps/training/components/UseEffect.jsx
@@ -4,12 +4,17 @@ function UseEffect() {
   const [post, setPost] = useState({ title: "Title", body: "Body" });
   const [searchQuery, setSearchQuery] = useState(1);
   console.log("UseEffect Rendered");
+
+  const fetchPost = async () => {
+    let post = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${searchQuery}`
+    );
+    post = await post.json();
+    await setPost(post);
+  };
+
   useEffect(() => {
-    return async () => {
-      let post = await fetch(`https://jsonplaceholder.typicode.com/posts/${searchQuery}`)
-      post = await post.json()
-      await setPost(post)
-    };
+    if(searchQuery.length) fetchPost();
   }, [searchQuery]);
   return (
     <div>
@@ -22,6 +27,7 @@ function UseEffect() {
               value={searchQuery}
               onChange={e => setSearchQuery(e.target.value)}
             />
+            <hr />
             {post.title}
             <hr />
             {post.body}
